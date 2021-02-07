be95419a739bd12284af455556c27d1b59f7f18e
hadtrindade <haddleytrindade@gmail.com>
2021-02-07 02:19:41 -0300

    funções integradas na GUI. close #5

--- fifo_simulator/app.py
+++ fifo_simulator/app.py
@@ -1,12 +1,63 @@
 import sys
 from PySide2 import QtWidgets
 from ui_fifo_simulator import Ui_FifoSimulator
+from fifo import queue_fifo, get_full_queue, processing
+from worker import Worker
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_FifoSimulator):
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
         self.setupUi(self)
+        self.queue_process = get_full_queue()
+        self.queue = queue_fifo
+        self.start = False
+        self.worker = None
+        self.button_start_stop.clicked.connect(lambda: self.start_process())
+        self.progress_bar_value(0)
+        self.label_p1.setText("P0")
+        self.label_p2.setText("P1")
+        self.label_p3.setText("P2")
+
+    def start_process(self):
+        if not self.start:
+            self.button_start_stop.setText("Stop")
+            self.start = True
+            self.worker = Worker(processing)
+            self.worker.signal.list_process.connect(self.set_queue)
+            self.worker.signal.progress_signal.connect(self.progress_bar_value)
+            self.worker.signal.progress_signal_percentage.connect(
+                self.label_cpu_percentage.setText
+            )
+            self.worker.start()
+
+        else:
+            self.worker.terminate()
+            self.button_start_stop.setText("Start")
+            self.start = False
+            self.progress_bar_value(0)
+            self.label_cpu_percentage.setText("0%")
+
+    def set_queue(self, list_queue):
+        queue_process_label = [self.label_p1, self.label_p2, self.label_p3]
+        count = 0
+        for i in range(list_queue[0], list_queue[0] + 3):
+            queue_process_label[count].setText(f"P{str(i)}")
+            count += 1
+
+    def progress_bar_value(self, value):
+
+        progress = (100 - value) / 100.0
+        stop_1 = str(progress - 0.001)
+        stop_2 = str(progress)
+        style_sheet = (
+            "QFrame {"
+            "border-radius: 150px;"
+            f"background-color: qconicalgradient(cx:0.5, cy:0.5, angle:90, stop:{stop_1}"
+            f" rgba(211, 215, 207, 0), stop:{stop_2} rgba(136, 138, 133, 255));"
+            "}"
+        )
+        self.circular_progress.setStyleSheet(style_sheet)
 
 
 if __name__ == "__main__":
--- fifo_simulator/fifo.py
+++ fifo_simulator/fifo.py
@@ -1,6 +1,8 @@
 from queue import Queue
 from random import randint
 from functools import wraps
+from time import sleep
+from os import getpid
 
 
 queue_fifo = Queue(maxsize=3)
@@ -12,6 +14,7 @@ def coroutine(func):
         gen = func()
         next(gen)
         return gen
+
     return primer
 
 
@@ -27,3 +30,20 @@ def get_full_queue():
             queue_fifo.put(next(gen_next_process()))
         else:
             return list(queue_fifo.queue)
+
+
+def processing(
+    progress_signal_percentage=None,
+    progress_signal=None,
+    list_process=None,
+):
+    count = 0
+    while True:
+        value = queue_fifo.get()
+        list_process.emit((count, get_full_queue()))
+        print(count)
+        for i in range(value):
+            sleep(0.5)
+            progress_signal.emit(i / value * 100)
+            progress_signal_percentage.emit(str(round(i / value * 100)) + "%")
+        count += 1
